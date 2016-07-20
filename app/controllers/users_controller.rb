abc96fec999208415ad9b5b0825c8f0a7a0da1e3
sathiyaseelan <ssstarlet@gmail.com>
2016-07-20 19:36:01 +0530

    temp commit of search lookup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
-    
+
     def mygroups
-        @groups = current_user.groups
+      @groups = current_user.groups
     end
-    
+
     def search
-        render 'find_user'
+      search_param = params[:search_param]
+      @users = User.find_users(search_param)
+      render partial: 'users/lookup'
     end
+=begin
+    def search
+      if params[:search_param] == nil || params[:search_param] == ''
+        render :json => {error: 'Missing Search Parameter'},
+        status: 404
+      else
+        search_param = params[:search_param]
+        users = User.find_users(search_param)
+        render :json => users, status: :ok
+      end
+    end
+=end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,:lockable,
          :recoverable, :rememberable, :trackable, :validatable
-  
-  
+
+
     def groups
         Group.with_role(Role.group_roles, self)
     end
+
+    def full_name
+      "#{first_name} #{last_name}".strip if first_name || last_name
+    end
+    
+    def self.find_users(search_param)
+      User.where("email like ? or first_name like ? or last_name like ?","%#{search_param}%","%#{search_param}%","%#{search_param}%")
+    end
+
 
 end
